816d5841364c30a35b45250246ac2881153e0520
Divya Sonawane <divyasonawane8724@gmail.com>
2026-08-15 10:27:57 +0530

    Add canonical URL for Placement Spark

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -97,6 +97,13 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
+
+      // Canonical URL
+  {
+    rel: "canonical",
+    href: "https://www.placementspark.com/",
+  },
+
       { rel: "icon", href: "/favicon.png", type: "image/png" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
